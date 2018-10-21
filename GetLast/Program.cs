c3425e0503aa9e641759c51e151a432ebfdba1f3
Krutonium <PFCKrutonium@gmail.com>
2018-10-20 22:40:16 -0400

    Code Dump. Finds last modified file in a directory.

--- GetLast/Program.cs
+++ GetLast/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace GetLast
 {
@@ -6,7 +8,35 @@ namespace GetLast
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                string ARG = args[0].ToUpper();
+                var AllFiles = Directory.GetFiles("./");
+                List<string> Validfiles = new List<string>();
+                foreach(var file in AllFiles)
+                {
+                    if (file.ToUpper().EndsWith(ARG))
+                    {
+                        Validfiles.Add(file);
+                    }
+                }
+                string MostRecentFile="No File Found.";
+                DateTime modifiedscratch = DateTime.MinValue;
+                foreach(var file in Validfiles)
+                {
+                    if (File.GetLastWriteTime(file) > modifiedscratch)
+                    {
+                        MostRecentFile = file;
+                        modifiedscratch = File.GetLastWriteTime(file);
+                    }
+                }
+                
+                Console.WriteLine(Path.GetFullPath(MostRecentFile));
+            }
+            catch
+            {
+                Console.WriteLine("Args: GetLast <file extention>");
+            }
         }
     }
 }
